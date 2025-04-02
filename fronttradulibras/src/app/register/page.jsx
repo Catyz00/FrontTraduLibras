3ca340b8206416487ce9c1ca23c332dba2885859
Catarina Dalsan Escolar Ribeiro <125068498+Catyz00@users.noreply.github.com>
2025-04-02 16:15:39 -0300

    Update ajustes na pag registro

--- fronttradulibras/src/app/register/page.jsx
+++ fronttradulibras/src/app/register/page.jsx
@@ -2,7 +2,7 @@ import { RegisterForm } from '@/components/register-form';
 
 export default function RegisterPage() {
   return (
-    <div className=" flex min-h-svh flex-col items-center justify-center p-6 md:p-10 bg-[#a3c8ff]">
+    <div className=" flex min-h-svh flex-col items-center justify-center p-6 md:p-10 bg-[#3B3F46]">
       <div className="w-full max-w-sm md:max-w-3xl">
         <RegisterForm />
       </div>
