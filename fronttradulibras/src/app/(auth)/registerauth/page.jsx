d9aa6c4c837ccf6599e326e5fde5ddd409052520
OLemonPie <gustavodutra122@gmail.com>
2025-04-20 17:52:10 -0300

    There was 2 login and register pages
    I changed one of them to loginauth and registerauth

--- fronttradulibras/src/app/(auth)/registerauth/page.jsx
+++ fronttradulibras/src/app/(auth)/registerauth/page.jsx
@@ -1,6 +1,6 @@
 import { RegisterForm } from '@/components/register/register-form';
 
-export default function RegisterPage() {
+export default function RRegisterPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10 bg-[#3B3F46]">
       <div className="w-full max-w-sm md:max-w-3xl">
